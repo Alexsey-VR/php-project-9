aa510b4ed5a0669ecac2bced922b993511a2cebe
Alexsey-VR <vityaz.iss@yandex.ru>
2026-04-07 13:15:48 +0000

    updated database structure

--- database.sql
+++ database.sql
@@ -1,17 +1,18 @@
+DROP TABLE url_checks;
+DROP TABLE urls;
+
 CREATE TABLE urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(255) UNIQUE,
+    name VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE url_checks (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id BIGINT REFERENCES urls(id) ON DELETE CASCADE,
+    url_id BIGINT REFERENCES urls(id) NOT NULL,
     status INTEGER,
-    h1 VARCHAR(255),
-    title VARCHAR(255),
-    description VARCHAR(255),
+    h1 VARCHAR(1024),
+    title VARCHAR(1024),
+    description VARCHAR(1024),
     created_at TIMESTAMP NOT NULL
 );
-
-SET CLIENT_ENCODING TO 'UTF8';
