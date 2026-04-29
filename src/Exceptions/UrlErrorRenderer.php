49c813b9b252af4d2fd0ae7595511157d450de17
Alexsey-VR <vityaz.iss@yandex.ru>
2026-04-29 03:40:34 +0000

    updated url error renderer

--- src/Exceptions/UrlErrorRenderer.php
+++ src/Exceptions/UrlErrorRenderer.php
@@ -17,37 +17,25 @@ class UrlErrorRenderer extends AbstractErrorRenderer
     public function __invoke(Throwable $exception, bool $displayErrorDetails): string
     {
         if ($displayErrorDetails) {
-            $exceptionFragment = '<p>The application could not run because of the following error:</p>';
-            $exceptionFragment .= '<h2>Details</h2>';
-            $exceptionFragment .= $this->renderExceptionFragment($exception);
-        } else {
-            $exceptionFragment = "<p>{$this->getErrorDescription($exception)}</p>";
+            $type = get_class($exception);
+            $code = $exception->getCode();
+            $message = htmlentities($exception->getMessage());
+            $file = $exception->getFile();
+            $line = $exception->getLine();
+            $trace = htmlentities($exception->getTraceAsString());
         }
 
         $params = [
-            'exceptionFragment' => $exceptionFragment
+            'details' => $displayErrorDetails,
+            'type' => $type,
+            'code' => $code,
+            'message' => $message,
+            'file' => $file,
+            'line' => $line,
+            'trace' => $trace
         ];
 
         return $this->renderer->fetch('/Exceptions/urlException.phtml', $params);
-    }
-
-    private function renderExceptionFragment(Throwable $exception): string
-    {
-        $html = sprintf('<div><strong>Type:</strong> %s</div>', get_class($exception));
-
-        $code = $exception->getCode();
-        $html .= sprintf('<div><strong>Code:</strong> %s</div>', $code);
-
-        $html .= sprintf('<div><strong>Message:</strong> %s</div>', htmlentities($exception->getMessage()));
-
-        $html .= sprintf('<div><strong>File:</strong> %s</div>', $exception->getFile());
-
-        $html .= sprintf('<div><strong>Line:</strong> %s</div>', $exception->getLine());
-
-        $html .= '<h2>Trace</h2>';
-        $html .= sprintf('<pre>%s</pre>', htmlentities($exception->getTraceAsString()));
-
-        return $html;
     }
 
     public function setRenderer(PhpRenderer $renderer): void
