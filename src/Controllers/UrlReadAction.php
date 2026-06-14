dbca33f1120096220c1bfd58273232326617839f
Alexsey-VR <vityaz.iss@yandex.ru>
2026-06-14 16:08:00 +0000

    updated url read action

--- src/Controllers/UrlReadAction.php
+++ src/Controllers/UrlReadAction.php
@@ -9,6 +9,10 @@ use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Analyzer\Repository\{ValidatedUrlRepository, UrlCheckRepository};
 use Analyzer\Interfaces\UrlInterface;
+use Analyzer\Interfaces\AppExceptionInterface;
+use Analyzer\Exceptions\UrlsReadActionException;
+use Analyzer\Exceptions\{UrlException, UrlRepositoryException};
+use Analyzer\Exceptions\{UrlCheckException, UrlCheckRepositoryException};
 
 class UrlReadAction
 {
@@ -37,50 +41,79 @@ class UrlReadAction
         ResponseInterface $response,
         array $args
     ): PsrResponseInterface {
-        $id = $args['id'];
+        try {
+            $id = $args['id'];
 
-        $url = $this->urlRepository->find($id);
-        $messages = $this->flash->getMessages();
-        $checks = $this->urlCheckRepository->getEntitiesByUrlId($id);
-        $checkItems = [];
-        foreach ($checks as $check) {
-            $checkItems[] = [
-                'id' => $check->getId(),
-                'status' => $check->getStatus(),
-                'h1' => $check->getH1(),
-                'title' => $check->getTitle(),
-                'description' => $check->getDescription(),
-                'timestamp' => $check->getTimestamp()
+            $url = $this->urlRepository->find($id);
+            $messages = $this->flash->getMessages();
+            $checks = $this->urlCheckRepository->getEntitiesByUrlId($id);
+            $checkItems = [];
+            foreach ($checks as $check) {
+                $checkItems[] = [
+                    'id' => $check->getId(),
+                    'status' => $check->getStatus(),
+                    'h1' => $check->getH1(),
+                    'title' => $check->getTitle(),
+                    'description' => $check->getDescription(),
+                    'timestamp' => $check->getTimestamp()
+                ];
+            }
+
+            $params = [
+                'name' => ($url instanceof UrlInterface) ? $url->getUrl() : '',
+                'id' => ($url instanceof UrlInterface) ? $url->getId() : '',
+                'timestamp' => ($url instanceof UrlInterface) ? $url->getTimestamp() : '',
+                'messages' => $messages,
+                'checks' => $checkItems
             ];
-        }
 
-        $params = [
-            'name' => ($url instanceof UrlInterface) ? $url->getUrl() : '',
-            'id' => ($url instanceof UrlInterface) ? $url->getId() : '',
-            'timestamp' => ($url instanceof UrlInterface) ? $url->getTimestamp() : '',
-            'messages' => $messages,
-            'checks' => $checkItems
-        ];
+            if (!is_null($url)) {
+                return $this->renderer
+                    ->render(
+                        $response,
+                        'Urls/url.phtml',
+                        $params
+                    );
+            }
 
-        if (!is_null($url)) {
-            return $this->renderer
-                ->render(
-                    $response,
-                    'Urls/url.phtml',
-                    $params
+            $params = [
+                'details' => false,
+                'code' => 404,
+                'message' => 'Not found',
+            ];
+
+            return $this->renderer->render(
+                $response->withStatus(404),
+                '/Exceptions/urlException.phtml',
+                $params
+            );
+        } catch (AppExceptionInterface $exception) {
+            $data = file_get_contents(__DIR__ . "/../Exceptions/errorCodesInfo.json");
+            $errorCodesInfo = json_decode(
+                $data ?: '',
+                flags:JSON_OBJECT_AS_ARRAY
+            );
+            $errorCode = strval($exception->getErrorCode());
+            $debugMessage = $errorCodesInfo[$errorCode];
+
+            if (
+                $exception instanceof UrlException ||
+                $exception instanceof UrlRepositoryException ||
+                $exception instanceof UrlCheckException ||
+                $exception instanceof UrlCheckRepositoryException
+            ) {
+                throw new UrlsReadActionException(
+                    $debugMessage,
+                    intval(mb_substr($errorCode, 0, 3)),
+                    $exception
                 );
+            }
+
+            throw new UrlsReadActionException(
+                "Неизвестная ошибка",
+                intval(mb_substr($errorCode, 0, 3)),
+                $exception
+            );
         }
-
-        $params = [
-            'details' => false,
-            'code' => 404,
-            'message' => 'Not found',
-        ];
-
-        return $this->renderer->render(
-            $response->withStatus(404),
-            '/Exceptions/urlException.phtml',
-            $params
-        );
     }
 }
